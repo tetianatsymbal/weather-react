0f005be2ab38c3b253d76629299b5c89af39380b
Tetiana Tsymbal <77539726+tetianatsymbal@users.noreply.github.com>
2022-10-13 19:18:46 +0300

    create Weather.js

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+import "./components/styles.css";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Temperature from "./components/Temperature";
+import Forecast from "./components/Forecast";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="cardPage">
+        <Header />
+        <Form />
+        <Temperature />
+        <Forecast />
+      </div>
+    </div>
+  );
+}
